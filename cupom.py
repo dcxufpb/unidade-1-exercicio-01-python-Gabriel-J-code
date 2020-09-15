d0300b550b18f1a60ed5d3195bc51cd9a42906eb
Gabriel-J-code <67019722+Gabriel-J-code@users.noreply.github.com>
2020-09-15 18:40:47 -0300

    Update cupom.py

--- cupom.py
+++ cupom.py
@@ -12,11 +12,11 @@ cnpj = "42.591.651/0797-34"
 inscricao_estadual = "244.898.500.113"
 
 def imprime_dados_loja():
-    dados = (nome_loja)
-	dados += ("%s, %d %s" % (logradouro, numero, complemento))
-	dados += ("%s - %s - %s" % (bairro, municipio, estado))	
-	dados += ("CEP:%s Tel %s" % (cep, telefone))
-	dados += (observacao)
-	dados += ("CNPJ: %s" % cnpj)
-	dados += ("IE: %s" % inscricao_estadual)
-    return dados
+	dados = nome_loja + "\n"
+	dados += ("%s, %d %s" % (logradouro, numero, complemento))+ "\n"
+	dados += ("%s - %s - %s" % (bairro, municipio, estado))+ "\n"	
+	dados += ("CEP:%s Tel %s" % (cep, telefone))+ "\n"
+	dados += (observacao)+ "\n"
+	dados += ("CNPJ: %s" % cnpj)+ "\n"
+	dados += ("IE: %s" % inscricao_estadual)+ "\n"
+	return dados
